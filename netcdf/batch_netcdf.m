3a5d416620336e9e447d69d38ff7df54b46b5a56
Your Nameaskazik <askazik@mail.ru>
2017-06-01 14:33:33 +0400

    Worked data saving into netcdf file with group

--- netcdf/batch_netcdf.m
+++ netcdf/batch_netcdf.m
@@ -5,68 +5,81 @@ function batch_netcdf
 % 3. Если промежуток больше 10 с. (время неопределенности формата),
 % записывается новый временной массив.
 
-clc
-
 dt = 0.0002505;     % Время между отсчётами в секундах
+
+clc
 workdir = uigetdir('d:\!data\kuleshov\', 'Проверяемая папка...');
 
-dirlist = dir([workdir, '\T*.*']);
-s_in_day = 24 * 3600;
-N = 2621440;
-
 % Создание netCDF файла.
-nc_file = [workdir, '\data.nc'];
-% nccreate(nc_file,'v_amplitude',...
-%           'Dimensions',{'d_time',inf},...
-%           'Datatype','int16',...
-%           'DeflateLevel',9,...
-%           'Format','netcdf4_classic');
-% ncdisp(nc_file);
+try
+    cmode = bitor(netcdf.getConstant('CLOBBER'), netcdf.getConstant('NETCDF4'));
+    ncid = netcdf.create([workdir, '\directory.nc'], cmode);
 
-% cmode = bitor(netcdf.getConstant('CLOBBER'), ...
-%     netcdf.getConstant('64BIT_OFFSET'));
-cmode = bitor(netcdf.getConstant('CLOBBER'), netcdf.getConstant('NETCDF4'));
-%cmode = bitor(cmode, netcdf.getConstant('CLASSIC_MODEL'));
-ncid = netcdf.create(nc_file, cmode);
-
-% define dimensions
-time_dimid = netcdf.defDim(ncid,'time',netcdf.getConstant('NC_UNLIMITED'));
-
-% define variables
-varid = netcdf.defVar(ncid,'amplitude','NC_SHORT',[time_dimid]);
-netcdf.defVarDeflate(ncid,varid,true,true,9);
-
-% Create an attribute associated with the variable.
-netcdf.putAtt(ncid,0,'dt',dt);
-
-netcdf.endDef(ncid);
-ncdisp(nc_file)
-
-% Перебор по файлам в рабочей папке и внесение данных в хранилище.
-prevEnd = 0;
-prevFile = '';
-for i = 1:length(dirlist)
-    unit = dirlist(i);
-    fullname = [workdir, '\', unit.name];
-       
-    if unit.bytes/2 < N % Сохранён полный файл данных
-        disp(['Файл <',unit.name,'> (',num2str(unit.bytes),' bytes) - меньшего размера!'])
-    end
+    % Create an global attribute.
+    netcdf.putAtt(ncid,netcdf.getConstant('NC_GLOBAL'),'dt',dt);
     
-    begTime = getTimeFromFName(unit.name);
-    dT = dt * (unit.bytes/2 - 1) / s_in_day; % длительность файла в сутках
-    endTime = begTime + dT;
+    dirlist = dir([workdir, '\T*.*']);
+    s_in_day = 24 * 3600;
     
-    if prevEnd > 0
-        cur_delay = (begTime - prevEnd) * s_in_day;
-        if abs(cur_delay) > 10
-            disp([num2str(i),': ',prevFile,' -> ',unit.name,', ', num2str(cur_delay/60), ' мин, последовательность разорвана!'])
+    % Перебор по файлам в рабочей папке и внесение данных в хранилище.
+    prevEnd = 0;
+    prevFile = '';
+    key_new_group = true;
+    for i = 1:length(dirlist)
+        unit = dirlist(i);
+        if unit.bytes == 0 % пропускаем файлы нулевого размера
+            continue
         end
+        
+        begTime = getTimeFromFName(unit.name);
+        dT = dt * (unit.bytes/2 - 1) / s_in_day; % длительность файла в сутках
+        endTime = begTime + dT;
+        
+        if prevEnd > 0 % определение разрыва оси времени
+            cur_delay = (begTime - prevEnd) * s_in_day;
+            if abs(cur_delay) > 10
+                disp([num2str(i),': ',prevFile,' -> ',unit.name,', ', num2str(cur_delay/60), ' мин, последовательность разорвана!'])
+                key_new_group = true;
+            end
+        end
+        
+        % создание новой группы при наличие разрыва времени
+        if key_new_group
+            group_name = datestr(begTime,'yyyy-mm-dd_HH-MM-SS');
+            childGroupId = netcdf.defGrp(ncid,group_name);
+            % define dimensions
+            time_dimid = netcdf.defDim(childGroupId,'time',netcdf.getConstant('NC_UNLIMITED'));
+            % define variables
+            varid = netcdf.defVar(childGroupId,'amplitude','short',time_dimid);
+            netcdf.defVarDeflate(childGroupId,varid,true,true,9);
+            % Leave define mode and enter data mode to write data.
+            netcdf.endDef(childGroupId);
+            
+            key_new_group = false;
+            start = 0;
+        end
+        
+        % Извлечём данные из текущего файла
+        fullname = [workdir, '\', unit.name];
+        fid = fopen(fullname,'rb');
+        if fid
+            ampl = fread(fid,inf,'int16=>int16');
+            fclose(fid);
+        end
+        count = length(ampl);
+        netcdf.putVar(childGroupId,varid,start,count,ampl)
+        start = start + count;
+        
+        prevEnd = endTime;
+        prevFile = unit.name;
+        
     end
-    prevEnd = endTime;
-    prevFile = unit.name;
+    
+catch ME
+    warning(ME.message)
 end
 netcdf.close(ncid)
+ncdisp([workdir, '\directory.nc'])
 
 function begTime = getTimeFromFName(fname)
 % Извлечение даты выборки из имени файла
@@ -93,5 +106,4 @@ min = str2num(fname(10:11));
 % Выделим десятки секунд
 sec = 6 * str2num(fname(12));
                     
-begTime = datenum(year, month, day, hour, min, sec);
-         
+begTime = datenum(year, month, day, hour, min, sec);        
