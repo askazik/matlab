7693fb2c46486efb913aec2d27a60e3eeea02d5d
Alexei Skazik <akazik@mail.ru>
2018-04-08 18:37:51 +0300

    Fixed main window and ini_gui part. Fixed listbox window for direct work with files in directory.

---
+++ test.m
@@ -0,0 +1,18 @@
+%% 
+% Cleaning from previous life.
+clc
+clear
+
+% Set folders with source code.
+addpath('./')
+addpath('./gui')
+addpath('./class')
+
+%% Test classes
+% IniDB class
+db = db_mysql('ini_sources.mat');
+conn = openConnection(db);
+schemas = getSchemas(db);
+
+dialog = choose_list_item(schemas,'ionosphere','Choose a schema from popup list:');
+choice = dialog.getChoice()
